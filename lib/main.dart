607e72f5ae1442095af68c8dc468c5dc059c66e5
Ketan Shukla <ketankshukla@gmail.com>
2021-06-05 01:32:18 -0700

    screen shots updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:practice_ui/app_three.dart';
+import 'package:practice_ui/app_one.dart';
 
 void main() {
-  runApp(App3());
+  runApp(App1());
 }
