5a88f749645f2ec4d27b4ba377db413f70e956e2
Ketan Shukla <ketankshukla@gmail.com>
2021-06-05 01:23:22 -0700

    added debug line

--- lib/app_three.dart
+++ lib/app_three.dart
@@ -4,6 +4,7 @@ class App3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Column(
